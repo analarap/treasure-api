ecf822269495a8e39597ab3b80cbc8d7f80c29c6
unknown <analarapupo@gmail.com>
2024-12-09 20:02:15 -0300

    initial commit

---
+++ src/main/java/com/love/gusana/GusanaApplication.java
@@ -0,0 +1,13 @@
+package com.love.gusana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GusanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GusanaApplication.class, args);
+	}
+
+}
