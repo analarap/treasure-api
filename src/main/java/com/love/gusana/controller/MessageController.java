6528a6752318d707a02c737471c49be1fb186853
unknown <analarapupo@gmail.com>
2024-12-09 20:25:02 -0300

    feat: initial architecture implemented

---
+++ src/main/java/com/love/gusana/controller/MessageController.java
@@ -0,0 +1,10 @@
+package com.love.gusana.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/messages")
+public class MessageController {
+
+}
