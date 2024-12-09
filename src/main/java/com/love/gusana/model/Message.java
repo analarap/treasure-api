8b2c6359e54e3e40dafc4317327b32408b4c11ed
unknown <analarapupo@gmail.com>
2024-12-09 20:21:08 -0300

    feat: entity message added

---
+++ src/main/java/com/love/gusana/model/Message.java
@@ -0,0 +1,27 @@
+package com.love.gusana.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "mensagens")
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 500)
+    private String content;
+
+    @Column(nullable = false, length = 100)
+    private String author;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime dateCreation;
+}
