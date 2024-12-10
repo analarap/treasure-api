390e5ea03297f41b7b3bc70eea64300b091722f4
unknown <analarapupo@gmail.com>
2024-12-09 23:14:16 -0300

    feat: controller endpoints created

--- src/main/java/com/love/gusana/controller/MessageController.java
+++ src/main/java/com/love/gusana/controller/MessageController.java
@@ -2,8 +2,11 @@ package com.love.gusana.controller;
 
 import com.love.gusana.model.Message;
 import com.love.gusana.service.MessageService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/messages")
@@ -17,22 +20,37 @@ public class MessageController {
 
     @PostMapping
     public ResponseEntity<Message> createMessage(@RequestBody Message message){
-
+        Message newMessage = messageService.saveMessage(message);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newMessage);
     }
 
     @GetMapping
-    public ResponseEntity<Message> searchMessageByID(@RequestBody Message message){
+    public ResponseEntity<List<Message>> getAllMessages(){
+        List<Message> messageList = messageService.listAll();
+        return ResponseEntity.ok(messageList);
+    }
 
+    @GetMapping
+    public ResponseEntity<Message> searchMessageByID(@PathVariable Long id){
+        return messageService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Message> updateMessage(@RequestBody Message message){
-
+    public ResponseEntity<Message> updateMessage(@PathVariable Long id, @RequestBody Message updatedMessage){
+        return messageService.updateMessage(id, updatedMessage)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Message> deleteMessage(@RequestBody Message message){
-
+    public ResponseEntity<Message> deleteMessage(@PathVariable Long id){
+        if (messageService.deleteMessage(id)) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 
 }
